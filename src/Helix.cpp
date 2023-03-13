7b2773c08e779d1afdaf07435aefa3f27461147f
radmir <r@mail.ru>
2023-03-14 00:26:33 +0500

    Реализован класс Helix

---
+++ src/Helix.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <stdexcept>
+#include "Helix.h"
+
+void Helix::setRadius(double r)
+{
+	if (r <= 0)
+	{
+		throw std::runtime_error("Radius of helix must be positive number!");
+	}
+
+	radius = r;
+}
+
+void Helix::setStep(double step)
+{
+	if (step <= 0)
+	{
+		throw std::runtime_error("Step of helix must be positive number!");
+	}
+
+	this->step = step;
+}
+
+Point Helix::getPoint(double t) const
+{
+	return { radius * std::cos(t), radius * std::sin(t), step * t / (2 * std::acos(-1.)) };
+}
+
+Vector Helix::getDerivative(double t) const
+{
+	return { -radius * std::sin(t), radius * std::cos(t), step / (2 * std::acos(-1.)) };
+}
---
+++ src/Helix.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Point.h"
+#include "ICurve.h"
+
+/**
+ * @brief Трёхмерная спираль
+*/
+class Helix :public ICurve
+{
+private:
+
+	/** Радиус спирали */
+	double radius = 1;
+
+	/** Шаг спирали по оси Z */
+	double step = 1;
+	
+public:
+
+	Helix() = default;
+
+	/**
+	 * Основной конструктор для создания экземпляра класса
+	 * @param r радиус спирали
+	 * @param step шаг спирали
+	*/
+	Helix(double r, double step)
+	{
+		setRadius(r);
+		setStep(step);
+	}
+
+	double getRadius() const { return radius; }
+
+	double getStep() const { return step; }
+
+	void setRadius(double r);
+
+	void setStep(double step);
+
+	Point getPoint(double t) const override;
+
+	Vector getDerivative(double t) const override;
+};
